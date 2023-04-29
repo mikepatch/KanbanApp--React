7849fad3b7cdca8a0fa2e0afc2d9011532e03d55
Michal <maxpoz3212@gmail.com>
2023-04-29 19:57:59 +0200

    Update modal styles

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -9,7 +9,7 @@ function Modal({ title, text, closeModal }) {
         <aside
             // eslint-disable-next-line react/jsx-props-no-spreading
             {...accessibleOnClick(closeModal)}
-            className="flex justify-center items-center absolute top-0 left-0 w-full h-screen bg-black bg-opacity-60 z-50"
+            className="flex justify-center items-center fixed top-0 left-0 w-full h-screen bg-black bg-opacity-60 z-50"
         >
             <section className="flex flex-col gap-6 py-8 px-10 bg-zinc-600 drop-shadow-sm rounded-md w-11/12 max-w-md animate-slide-from-l">
                 <h2>{title}</h2>
