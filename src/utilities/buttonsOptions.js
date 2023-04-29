500d8dc049abd5f5e414b6e770dd3bff4405db3c
Michal <maxpoz3212@gmail.com>
2023-04-29 19:58:10 +0200

    Update buttons styles

--- src/utilities/buttonsOptions.js
+++ src/utilities/buttonsOptions.js
@@ -1,5 +1,5 @@
 const buttonStyles =
-    'bg-blue-500 hover:bg-blue-600 py-1 px-4 rounded-full text-white ease-in-out duration-100';
+    'text-xs md:text-lg bg-blue-500 hover:bg-blue-600 py-1 px-4 rounded-full text-white ease-in-out duration-100';
 const removeButtonStyles =
     'absolute top-0 -right-5 hover:scale-125 text-sm text-red-500 cursor-pointer font-bold linear duration-100';
 // const className = 'hover:scale'
